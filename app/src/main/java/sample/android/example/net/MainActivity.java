3e54c01574e334e57451d9580251a5af8a258b75
Ryogo Ikumi <carp193@yahoo.co.jp>
2020-06-18 21:35:40 +0900

    Initial commit

---
+++ app/src/main/java/sample/android/example/net/MainActivity.java
@@ -0,0 +1,25 @@
+package sample.android.example.net;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    //表示させたいURLを記入
+    private String accsessUri = "https://www.google.com";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView)findViewById(R.id.webview);
+        //webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(accsessUri);
+    }
+}
